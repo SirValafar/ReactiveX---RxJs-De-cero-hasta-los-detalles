d6678180877d266bacd98c41fffd3516a7eb30a1
Arturo <arturo.molina.gallego@gmail.com>
2020-10-05 18:50:28 -0300

    ✅ 17. Terminar observables en cadena

--- src/Observables/16. Subscription y unsubscribe.ts
+++ src/Observables/16. Subscription y unsubscribe.ts
@@ -4,37 +4,29 @@ const observer: Observer<any> = {
     next:  valor => console.log('next: ', valor),
     error: error => console.warn('Error: ',error),
     complete: () => console.info('completado')
+
 };
 
 const intervalos$ = new Observable<number>( sub => {
     let count = 0 ;
 
- const interval = setInterval ( () => {
+ const interval =  setInterval ( () => {
                         count++;
                         sub.next( count );
-                        console.log( count );
                     },1000);
 
-    setTimeout(() =>{
-        sub.complete()
-    }, 2500);
-                
+
+
+                    
+
     return () => {
         clearInterval(interval);
         console.log('Intervalo destruido');
     }
-});
+})
 
-/* const subs = intervalos$.subscribe( num => console.log('Num:', num)); */
-const subs1 = intervalos$.subscribe( observer );
-const subs2 = intervalos$.subscribe( observer );
-const subs3 = intervalos$.subscribe( observer );
-
-subs1.add( subs2 )
-     .add( subs3 );
+const subs = intervalos$.subscribe( num => console.log('Num:', num));
 
 setTimeout(() => {
-    subs1.unsubscribe()
-
-    console.log('Completado TimeOut');
-}, 6000);
+    subs.unsubscribe()
+}, 3000);
